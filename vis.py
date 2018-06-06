7d29b7e8b904f492e711c3be368ab5bf5954f9e0
Louis de Beaumont <debeaumont.louis@gmail.com>
2018-06-06 14:29:52 +0100

    Visualise node paths

--- vis.py
+++ vis.py
@@ -1,6 +1,18 @@
+import argparse
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+
+def getx(s):
+  i = s.find(',') + 1
+  s = s[i:]
+  return float(s)
+
+def gety(s):
+  i = s.find(',')
+  s = s[:i]
+  return float(s)
 
 # Using Mercator Projection: https://matplotlib.org/basemap/users/merc.html
 m = Basemap(projection='merc',llcrnrlat=51.520691,urcrnrlat=51.543877,\
@@ -15,6 +27,25 @@ xs = [-0.161837,-0.168247,-0.168230,-0.167499,-0.159788,-0.155579,-0.145723,-0.1
 ys = [51.54159,51.539064,51.534368,51.530447,51.526168,51.523623,51.525279,51.53104,51.536116,51.538872,51.54159]
 
 m.plot(xs,ys,'o-', markersize=5, linewidth=1, latlon=True)
+
+parser = argparse.ArgumentParser(description="Simulation Visualiser", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("file", type=argparse.FileType('r'), help="Simulation output to be visualised")
+args = parser.parse_args()
+
+# Parse and print node paths
+for line in args.file:
+  if "'points'" in line:
+    i = line.find("'points':") + 11
+    j = line.find(", '", i) - 1
+    line = line[:j]
+    line = line[i:]
+    line = line.replace(", -", ",-").replace(")","").replace("(","")
+    coords = line.split(", ")
+    
+    linexs = map(getx, coords)
+    lineys = map(gety, coords)
+
+    m.plot(linexs, lineys, latlon=True)
 
 plt.title("Mercator Projection")
 plt.show()
